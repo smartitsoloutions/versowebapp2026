c451e08f9529624e0e466123439b885c8abb60c1
smartitsoloutions <prasads128312@gmail.com>
2026-01-30 12:03:50 +0530

    this is my 3rd update

--- javacode.java
+++ javacode.java
@@ -4,7 +4,7 @@ public class UserInputExample {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter your name: ");
+        System.out.print("Enter Student Full Name: ");
         String name = scanner.nextLine();
 
         System.out.println("Hello World, " + name + "!");
